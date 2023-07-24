0bf9bd2b96e218c51df2454f3eb48781378f9b8c
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-07-23 23:40:53 -0300

    style: aplicando linter e formatter

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,29 @@
 import Hapi from '@hapi/hapi';
 
 const serverInit = async () => {
-    const server = Hapi.server({
-        port: 3333,
-        host: 'localhost',
-    });
-    
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler() {
-            return 'It worked!'
-        }
-    })
-    
-    await server.start();
-    
-    console.log('Server running on %s', server.info.uri);
-}
+  const server = Hapi.server({
+    port: 3333,
+    host: 'localhost',
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler() {
+      return 'It worked!';
+    },
+  });
+
+  await server.start();
+
+  // rome-ignore lint/nursery/noConsoleLog: <explanation>
+  console.log('Server running on %s', server.info.uri);
+};
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
-})
+  // rome-ignore lint/nursery/noConsoleLog: <explanation>
+  console.log(err);
+  process.exit(1);
+});
 
 serverInit();
