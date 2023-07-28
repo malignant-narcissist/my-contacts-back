4c51213ecc0a50de67ddb61f80c709eed8f67f28
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-07-28 20:14:44 -0300

    feat: indexando nova rota

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,6 +7,11 @@ const routes: ServerRoute[] = [
     path: '/contacts',
     handler: Controller.index,
   },
+  {
+    method: 'GET',
+    path: '/contacts/{id}',
+    handler: Controller.show,
+  },
 ];
 
 export { routes };
