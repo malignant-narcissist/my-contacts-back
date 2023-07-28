d6ef79a0e98037cd660d9b4cbdca3d6aaeb4ec67
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-07-28 19:47:34 -0300

    feat: criando recurso de encontrar contato por ID

--- src/app/repositories/ContactsRepositories.ts
+++ src/app/repositories/ContactsRepositories.ts
@@ -10,6 +10,14 @@ class ContactsRepository {
   async findAll() {
     return Array.from(this.contacts.values());
   }
+
+  async findById(id: string) {
+    const contact = Array.from(this.contacts).find((i) => {
+      return i.id === id;
+    });
+
+    return contact;
+  }
 }
 
 const contactsRepository = new ContactsRepository();
