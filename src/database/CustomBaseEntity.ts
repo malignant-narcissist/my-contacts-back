c9d236afc165207f963c04fe79b51e470238d6d9
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-08-22 19:44:28 -0300

    feat: criando entidade compositiva base para outras entidades

---
+++ src/database/CustomBaseEntity.ts
@@ -0,0 +1,23 @@
+import { PrimaryKey, Property } from '@mikro-orm/core';
+import { monotonicFactory } from 'ulid';
+
+interface ICustomBaseEntity {
+  id: string;
+  createdAt: Date;
+  updatetAt: Date;
+}
+
+abstract class BaseEntity implements ICustomBaseEntity {
+  @PrimaryKey({
+    type: 'uuid',
+  })
+  id: string = monotonicFactory()();
+
+  @Property()
+  createdAt!: Date;
+
+  @Property()
+  updatetAt!: Date;
+}
+
+export { ICustomBaseEntity, BaseEntity };
