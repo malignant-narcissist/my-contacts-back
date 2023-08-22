4547e6b0788fbf3cc94a502eab537cea04015c79
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-08-22 19:44:28 -0300

    chore: criando instância do mikro-orm

---
+++ src/database/source.ts
@@ -0,0 +1,15 @@
+import config from './mikro-orm.config';
+import { MikroORM } from '@mikro-orm/core';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+
+let orm: MikroORM<PostgreSqlDriver>;
+
+MikroORM.init<PostgreSqlDriver>(config)
+  .then((instance) => {
+    orm = instance;
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+export { orm };
