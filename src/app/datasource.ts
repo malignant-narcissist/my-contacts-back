69cf60035483d20b504ffc0c53cbf9c818fd7353
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-09-03 22:34:15 -0300

    refactor: alterando nome do campo

--- src/app/datasource.ts
+++ src/app/datasource.ts
@@ -1,6 +1,6 @@
 import { monotonicFactory } from 'ulid';
 
-type ContactFields = 'id' | 'name' | 'email' | 'phone' | 'categoryId';
+type ContactFields = 'id' | 'name' | 'email' | 'phone' | 'category_id';
 
 type Contact<
   Key extends ContactFields = ContactFields,
@@ -12,13 +12,23 @@ type Contact<
 const contacts = new Map<string, Contact>();
 
 const contactOne: Contact = {
-  id: monotonicFactory()(),
+  id: '12',
   name: 'Josh Shadle',
-  categoryId: monotonicFactory()(),
+  category_id: monotonicFactory()(),
   email: 'josh.shadle@mail.com',
   phone: '(81) 98890-9310',
 };
 
+const contactTwo: Contact = {
+  id: '13',
+  name: 'Yusuf Akc',
+  category_id: monotonicFactory()(),
+  email: 'r@mail.com',
+  phone: '(81) 99876-9930',
+};
+
 contacts.set(contactOne.id, contactOne);
+
+contacts.set(contactTwo.id, contactTwo);
 
 export { contacts, Contact };
