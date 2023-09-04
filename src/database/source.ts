8ee4105aa1a2b081d940a00fe725315c64e467fb
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-09-03 22:32:04 -0300

    refactor: instanciando orm com await top-level
    Instanciando MikroORM como const e com await top-level

--- src/database/source.ts
+++ src/database/source.ts
@@ -2,14 +2,6 @@ import config from './mikro-orm.config.ts';
 import { MikroORM } from '@mikro-orm/core';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
-let orm: MikroORM<PostgreSqlDriver>;
+const mainOrm = await MikroORM.init<PostgreSqlDriver>(config);
 
-MikroORM.init<PostgreSqlDriver>(config)
-  .then((instance) => {
-    orm = instance;
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-export { orm };
+export { mainOrm };
