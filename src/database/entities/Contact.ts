14f79c7c94e387d631529d0c8219117a881203cb
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-09-04 21:43:21 -0300

    fix: corrigindo erro de acesso pré-inicialização

--- src/database/entities/Contact.ts
+++ src/database/entities/Contact.ts
@@ -1,6 +1,6 @@
 import { BaseEntity } from '../CustomBaseEntity.ts';
 import { Category } from './Category.ts';
-import { Entity, ManyToOne, Property } from '@mikro-orm/core';
+import { Entity, ManyToOne, Property, Rel } from '@mikro-orm/core';
 
 @Entity({
   tableName: 'contacts',
@@ -22,7 +22,7 @@ class Contact extends BaseEntity {
   @ManyToOne(() => Category, {
     nullable: true,
   })
-  category?: Category;
+  category?: Rel<Category>;
 }
 
 export { Contact };
