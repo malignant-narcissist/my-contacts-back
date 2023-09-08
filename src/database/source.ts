312cee042d8a5d1afcf1eca6cd48209825757480
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-09-08 19:40:35 -0300

    style: alterando ordem de import

--- src/database/source.ts
+++ src/database/source.ts
@@ -1,6 +1,6 @@
-import config from './mikro-orm.config.ts';
 import { MikroORM } from '@mikro-orm/core';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import config from './mikro-orm.config.ts';
 
 const mainOrm = await MikroORM.init<PostgreSqlDriver>(config);
 
